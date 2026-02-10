b9fc296e06474046bc9a18ff884a1ae3590335d4
Sebastian Villavicencio <Sebastianvill1003@gmail.com>
2026-02-09 22:03:23 -0500

    first commit

---
+++ strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args) {
+
+        //Practice initializing a string variable and assigning it a value
+       String name = "Sebastian Villavicencio";
+       System.out.print(name);
+    }
+}
